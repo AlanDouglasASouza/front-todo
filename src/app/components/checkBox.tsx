e56739d96841c2e297c3701b1f14c6e7cae624b3
Alan <alan.douglas.ads@gmail.com>
2023-07-24 23:27:19 -0300

    feat: update checkbox

--- src/app/components/checkBox.tsx
+++ src/app/components/checkBox.tsx
@@ -1,13 +1,13 @@
 export default function Checkbox({ label, checked, onChange }: any) {
-  return (
-    <label className="flex items-center space-x-2">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-        className="form-checkbox h-4 w-4 text-gray-700 transition duration-150 ease-in-out appearance-none border rounded-sm border-gray-900 bg-slate-600 checked:bg-[url('https://img.icons8.com/material-rounded/24/737373/checked-checkbox.png')]"
-      />
-      <span className="text-gray-100">{label}</span>
-    </label>
-  );
+    return (
+        <label className="flex items-center space-x-2">
+            <input
+                type="checkbox"
+                checked={checked}
+                onChange={onChange}
+                className="form-checkbox cursor-pointer h-4 w-4 text-gray-700 appearance-none border rounded-sm border-gray-900 bg-slate-600 bg-center checked:bg-[url('https://img.icons8.com/ios-glyphs/30/4D4D4D/checked-checkbox.png')] checked:bg-slate-50"
+            />
+            <span className="text-gray-100">{label}</span>
+        </label>
+    );
 }
