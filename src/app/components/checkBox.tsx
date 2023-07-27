bcf09e7bdb8c977ed6783b841c4b4fcee0a3a5d0
Alan Douglas <alan.souza_ext@stone.com.br>
2023-07-26 22:57:23 -0300

    chore(checkBox.tsx): remove checked in checkbox

--- src/app/components/checkBox.tsx
+++ src/app/components/checkBox.tsx
@@ -5,7 +5,6 @@ export default function Checkbox({ label, checked, onChange }: ICheckBox) {
     <label className="flex items-center space-x-2">
       <input
         type="checkbox"
-        checked={checked}
         onChange={onChange}
         className="form-checkbox cursor-pointer h-4 w-4 text-gray-default appearance-none border rounded-sm border-black-border bg-gray-modal-options bg-center checked:bg-[url('https://img.icons8.com/ios-glyphs/30/4D4D4D/checked-checkbox.png')] checked:bg-white-word"
       />
